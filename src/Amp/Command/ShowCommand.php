8d280998370a179999e5f3e5d31e28ac74411ad2
Tim Otten <totten@civicrm.org>
2013-12-14 20:07:27 -0800

    ShowCommand - Ensure default layout fits within screen. Add verbose mode.

--- src/Amp/Command/ShowCommand.php
+++ src/Amp/Command/ShowCommand.php
@@ -31,14 +31,39 @@ class ShowCommand extends ContainerAwareCommand {
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
-    $rows = array();
-    foreach ($this->instances->findAll() as $instance) {
-      $rows[] = array($instance->getRoot(), $instance->getName(), $instance->getDsn(), $instance->getUrl());
-    }
+    if ($output->getVerbosity() > OutputInterface::VERBOSITY_NORMAL) {
+      $rows = array();
+      foreach ($this->instances->findAll() as $instance) {
+        $rows[] = array('root', $instance->getRoot());
+        $rows[] = array('name', $instance->getName());
+        $rows[] = array('dsn', $instance->getDsn());
+        $rows[] = array('url', $instance->getUrl());
+        $rows[] = array('', '');
+      }
 
-    $table = $this->getApplication()->getHelperSet()->get('table');
-    $table->setHeaders(array('Root', 'Name', 'DSN', 'URL'));
-    $table->setRows($rows);
-    $table->render($output);
+      /** @var $table \Symfony\Component\Console\Helper\TableHelper */
+      $table = $this->getApplication()->getHelperSet()->get('table');
+      $table->setHeaders(array('Property', 'Value'));
+      $table->setRows($rows);
+      $table->render($output);
+    }
+    else {
+      $rows = array();
+      foreach ($this->instances->findAll() as $instance) {
+        $rows[] = array(
+          $instance->getRoot(),
+          $instance->getName(),
+          $instance->getDsn() ? 'y' : '',
+          $instance->getUrl() ? 'y' : '',
+        );
+      }
+
+      /** @var $table \Symfony\Component\Console\Helper\TableHelper */
+      $table = $this->getApplication()->getHelperSet()->get('table');
+      $table->setHeaders(array('Root', 'Name', 'DB', 'Web'));
+      $table->setRows($rows);
+      $table->render($output);
+      $output->writeln('For more detailed info, use "amp show -v" or "amp export [--root=X] [---name=X]');
+    }
   }
 }
