49e2a7c637ccb010450f9a6ed8065ffc5a5b0317
AbdealiJK <abdealikothari@gmail.com>
2016-03-01 11:32:07 +0530

    ExportCommand - Use container when needed
    This is one commit in the series of commits to move out the
    `$instances` from __construct methods everywhere.
    
    Related to https://github.com/totten/amp/issues/35

--- src/Amp/Application.php
+++ src/Amp/Application.php
@@ -152,7 +152,7 @@ class Application extends \Symfony\Component\Console\Application {
     $commands[] = new \Amp\Command\TestCommand($this, NULL);
     $commands[] = new \Amp\Command\CreateCommand($this, NULL);
     $commands[] = new \Amp\Command\ShowCommand($this, NULL);
-    $commands[] = new \Amp\Command\ExportCommand($this, NULL, $this->getContainer()->get('instances'));
+    $commands[] = new \Amp\Command\ExportCommand($this, NULL);
     $commands[] = new \Amp\Command\SqlCommand($this, NULL, $this->getContainer()->get('instances'));
     $commands[] = new \Amp\Command\DestroyCommand($this, NULL, $this->getContainer()->get('instances'));
     $commands[] = new \Amp\Command\CleanupCommand($this, NULL, $this->getContainer()->get('instances'));
