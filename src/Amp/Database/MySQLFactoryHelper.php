7054775882b0a6b8efb2ddf0267c353e55267621
Tim Otten <totten@civicrm.org>
2016-06-27 19:15:26 -0700

    MySQLFactoryHelper - Locate MySQL data files with MAMP or mysql.com builds
    If you have multiple mysqld's available, try to pick the config files which
    are appropriate to the active version.

--- src/Amp/Database/MySQLFactoryHelper.php
+++ src/Amp/Database/MySQLFactoryHelper.php
@@ -1,6 +1,7 @@
 <?php
 namespace Amp\Database;
 
+use Amp\Util\Shell;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Filesystem\Filesystem;
 
@@ -11,27 +12,41 @@ class MySQLFactoryHelper {
    * @return DatabaseManagementInterface
    */
   public static function createRAMServer(ContainerInterface $container) {
+    list ($mysqldBin) = Shell::run("which mysqld");
+
     $server = new MySQLRAMServer();
     $server->setRamDisk($container->get('ram_disk'));
     $server->setMySQLRamServerPort($container->getParameter('mysql_ram_server_port'));
-    $server->setDefaultDataFiles(self::findDataFiles());
+    $server->setDefaultDataFiles(self::findDataFiles($mysqldBin));
     if (file_exists("/etc/apparmor.d")) {
       $server->setAppArmor($container->get('app_armor.mysql_ram_disk'));
     }
     return $server;
   }
 
-  public static function findDataFiles() {
-    $filesets = array(
-      'mamp' => array(
+  public static function findDataFiles($mysqldBin) {
+    $filesets = array();
+
+    if (preg_match(';MAMP;', $mysqldBin)) {
+      $filesets['mamp'] = array(
         '/Applications/MAMP/Library/share/mysql_system_tables.sql',
         '/Applications/MAMP/Library/share/mysql_system_tables_data.sql',
-      ),
-      'debian' => array(
+      );
+    }
+
+    if (preg_match(';/usr/local/mysql.*/bin;', $mysqldBin)) {
+      $filesets['mysql.com'] = array(
+        '/usr/local/mysql/share/mysql_system_tables.sql',
+        '/usr/local/mysql/share/mysql_system_tables_data.sql',
+      );
+    }
+
+    if (preg_match(';/usr/bin;', $mysqldBin)) {
+      $filesets['debian'] = array(
         '/usr/share/mysql/mysql_system_tables.sql',
         '/usr/share/mysql/mysql_system_tables_data.sql',
-      ),
-    );
+      );
+    }
 
     $fs = new Filesystem();;
 
