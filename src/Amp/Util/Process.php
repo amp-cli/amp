c98755d87ae1baec8a3906197d3a05851d245736
Tim Otten <totten@civicrm.org>
2017-05-04 18:24:44 -0500

    Add Process::isShellScript()

--- src/Amp/Util/Process.php
+++ src/Amp/Util/Process.php
@@ -18,4 +18,16 @@ class Process {
     return FALSE;
   }
 
+  /**
+   * Determine if $file is a shell script.
+   *
+   * @param $file
+   * @return bool
+   */
+  public static function isShellScript($file) {
+    $firstLine = file_get_contents($file, NULL, NULL, 0, 120);
+    list($firstLine) = explode("\n", $firstLine);
+    return (bool) preg_match(';^#.*bin.*sh;', $firstLine);
+  }
+
 }
