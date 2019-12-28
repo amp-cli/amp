2f78509318f0cded47de19f4cdade762c83b804c
Seamus Lee <seamuslee001@gmail.com>
2019-12-29 09:37:06 +1100

    Split out create user from grant for MySQL8 compatability

--- src/Amp/Database/MySQL.php
+++ src/Amp/Database/MySQL.php
@@ -82,13 +82,17 @@ class MySQL implements DatabaseManagementInterface {
     $dbh->exec("CREATE DATABASE `$db`");
     switch ($perm) {
       case DatabaseManagementInterface::PERM_SUPER:
-        $dbh->exec("GRANT ALL ON *.* to '$user'@'localhost' IDENTIFIED BY '$pass' WITH GRANT OPTION");
-        $dbh->exec("GRANT ALL ON *.* to '$user'@'%' IDENTIFIED BY '$pass' WITH GRANT OPTION");
+        $dbh->exec("CREATE USER IF NOT EXISTS '$user'@'localhost' IDENTIFIED WITH mysql_native_password BY '$pass'");
+        $dbh->exec("CREATE USER IF NOT EXISTS '$user'@'%' IDENTIFIED WITH mysql_native_password BY '$pass'");
+        $dbh->exec("GRANT ALL ON *.* to '$user'@'localhost' WITH GRANT OPTION");
+        $dbh->exec("GRANT ALL ON *.* to '$user'@'%' WITH GRANT OPTION");
         break;
 
       case DatabaseManagementInterface::PERM_ADMIN:
-        $dbh->exec("GRANT ALL ON `$db`.* to '$user'@'localhost' IDENTIFIED BY '$pass'");
-        $dbh->exec("GRANT ALL ON `$db`.* to '$user'@'%' IDENTIFIED BY '$pass'");
+        $dbh->exec("CREATE USER IF NOT EXISTS '$user'@'localhost' IDENTIFIED WITH mysql_native_password BY '$pass'");
+        $dbh->exec("CREATE USER IF NOT EXISTS '$user'@'%' IDENTIFIED WITH mysql_native_password BY '$pass'");
+        $dbh->exec("GRANT ALL ON `$db`.* to '$user'@'localhost'");
+        $dbh->exec("GRANT ALL ON `$db`.* to '$user'@'%'");
         break;
 
       default:
